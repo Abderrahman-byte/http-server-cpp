f5f165b414d3b73ec79a3d7b1f2d7c077cf69658
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-08-14 19:36:57 +0100

    initialize threads and accept new connections

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,41 @@
-#include <iostream>
-#include <unistd.h>
+#include <thread>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <bits/refwrap.h>
 
-#include "server.hpp"
 #include "http_parser/parser.hpp"
+#include "connection.hpp"
+#include "server.hpp"
+#include "handleThread.hpp"
 
-#define BUFFER_SIZE 1024
+#define THREADS_COUNT 10
 
 int main () {
     int server_socket = start_server();
+    std::vector<Connection> connections;
+    std::thread threads_pool[THREADS_COUNT];
 
-    while (1) {
-        IncomingMessage_t request ;
-        char buffer[BUFFER_SIZE];
+    unsigned int accepted_connections = 0;
+    socklen_t address_len = sizeof(struct sockaddr_in);
 
-        struct sockaddr_in client_address;
-        unsigned int client_address_len = sizeof(client_address);
-
-        int client_socket = accept(server_socket, (sockaddr *)&client_address, &client_address_len);
-
-        int received_len = recv(client_socket, buffer, BUFFER_SIZE, 0);
-
-        std::cout << "[*] Received : " << received_len << "\n";
-
-        try {
-            parse_http(&request, buffer, received_len);
-        } catch(const char* e) {
-            std::cout << "[ERROR] : " << e << '\n';
-        }
-
-        close(client_socket);
+    // Initialize threads
+    for (int i = 0; i < THREADS_COUNT; i++) {
+        threads_pool[i] = std::thread(handleThread, std::ref(connections), i);
     }
-    
+
+    // Accept new connections
+    while (true) {
+        struct sockaddr_in client_address;
+        int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &address_len);
+
+        Connection conn(client_socket, client_address);
+        conn.worker_id = accepted_connections % THREADS_COUNT;
+        connections.push_back(conn);
+
+        accepted_connections++;
+    }
+
+    // Wait for threads to finish in case of interopt ?
+    for (int i = 0; i < THREADS_COUNT; i++) threads_pool[i].join();
+
     return 0;
 }
