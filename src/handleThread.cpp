79f901f4430e256cb02303eb395d9b02761faeeb
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-08-15 00:31:21 +0100

    Close connection if keep alive timeout passed

--- src/handleThread.cpp
+++ src/handleThread.cpp
@@ -1,9 +1,12 @@
 #include <unistd.h>
 #include <vector>
 #include <iostream>
+#include <ctime>
 
+#include "http_parser/parser.hpp"
 #include "connection.hpp"
 #include "handleThread.hpp"
+#include "handleRequest.hpp"
 
 void handleThread (std::vector<Connection> &connections, int i) {
     char buffer[BUFFER_SIZE];
@@ -13,18 +16,30 @@ void handleThread (std::vector<Connection> &connections, int i) {
         int connections_len = connections.size();
 
         for(int j = 0; j < connections_len; j++) {
-            if (connections[j].worker_id != i || !connections[j].is_open) continue;
- 
+            if (connections[j].worker_id != i || !connections[j].is_open()) continue;
+
+            if (connections[j].last_request > 0 &&
+                time(0) - connections[j].last_request >= KEEPALIVE_TIMEOUT) {
+                connections[j].close_connection();
+                continue;
+            }
+
             received_len = recv(connections[j].socket, buffer, BUFFER_SIZE, MSG_DONTWAIT);
             
-            if (received_len >= 0) {
+            if (received_len > 0) {
+                IncomingMessage_t request;
                 std::cout << "[*] From Thread [" << i << "] received : " << received_len << std::endl;
-            } else {
-                connections[j].close_connection();
+                
+                parse_http(&request, buffer, BUFFER_SIZE);
+                handleRequest(connections[i], request);
+                connections[j].last_request = time(0);
+
+                if (request.http_version.compare("HTTP/1.0") == 0) {
+                    connections[j].close_connection();
+                }
             }
-            
         }
     
-       usleep(100000);
+       usleep(SLEEP_TIME);
     }
 }
--- src/handleThread.hpp
+++ src/handleThread.hpp
@@ -5,6 +5,8 @@
 #ifndef _HANDLETHREAD_H_
 #define _HANDLETHREAD_H_
 
+#define KEEPALIVE_TIMEOUT 5
+#define SLEEP_TIME 100000
 #define BUFFER_SIZE 1024
 
 extern void handleThread (std::vector<Connection> &, int);
