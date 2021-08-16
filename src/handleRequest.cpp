48add2c532e76e40410f1db947fc7536fd3de165
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-08-16 12:38:26 +0100

    Serve static files

--- src/handleRequest.cpp
+++ src/handleRequest.cpp
@@ -1,46 +1,95 @@
 #include <string>
-#include <sys/socket.h>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fstream>
 
 #include "http_parser/parser.hpp"
+#include "response/response.hpp"
 #include "connection.hpp"
 #include "handleRequest.hpp"
 
-void handleRequest(Connection connection, IncomingMessage_t request) {
-    url_t request_url;
+void handleRequest(Connection& connection, IncomingMessage_t request) {
+    Response response;
+    url_t url;
     std::string response_str;
-    std::string body;
+    std::string cwd(getcwd(NULL, 0));
+    std::string requested_file;
+    
+    parse_url(&url, request.original_url);
+    requested_file = join_path(join_path(cwd, STATIC_DIR), url.path);
+    serve_static(&response, requested_file);
 
-    body += "<html>";
-    body += "<head>";
-    body += "<title>Abderrahmane</title>";
-    // body += "<script src='/static/main.js' ></script>";
-    // body += "<script src='/static/index.js' ></script>";
-    body += "</head>";
-    body += "<body>";
-    body += "<h1>Abderrahmane</h1>";
-    body += "</body>";
-    body += "</html>";
-
-    response_str += "HTTP/1.1 200 OK\r\n";
-    response_str += "Content-Type: text/html\r\n";
-    response_str += "Connection: keep-alive\r\n";
-    response_str += "Keep-Alive: timeout=5\r\n";
-    response_str += "Content-Length: " + std::to_string(body.length()) + "\r\n";
-    response_str += "Server: Abderrahmane\r\n";
-    response_str += "\r\n";
-    response_str += body;
-
-    parse_url(&request_url, request.original_url);
-
-    // For testing
-    // std::cout << "[*] Parsed url : " << request_url.path << std::endl;
-    // int s = request_url.searchquery.size();
-    // std::cout << "[*] Parsed query : " <<  s << std::endl;
-
-    // for (int i = 0; i < s ; i ++) {
-    //     std::cout << "\t- " << request_url.searchquery[i].name << " => " << request_url.searchquery[i].value  << std::endl; 
-    // } 
+    response.set_header("Server", "Abderrhmane");
+    response.set_header("X-Powered-By", "C++");
+    response_str = response.get_raw();
 
     send(connection.socket, response_str.c_str(), response_str.length(), MSG_DONTWAIT);
+}
+
+void serve_static(Response *response, std::string filename) {
+    struct stat filestat;
+    bool file_exists = check_file_exists(filename, &filestat);
+
+    if (!file_exists) {
+        not_found(response);
+        return ;
+    } 
+    
+    if (filestat.st_mode & S_IFDIR) {
+        serve_static(response, join_path(filename, "index.html"));
+    } else if (filestat.st_mode & S_IFREG) {
+        response->status_code = 200;
+        response->set_header("Content-Type", "text/html");
+        response->body = get_file_content(filename);  
+    } else not_found(response);
+}
+
+void not_found(Response *response) {
+    response->status_code = 404;
+    response->set_header("Content-Type", "text/plain");
+    response->body = "File doesn\'t exist";  
+}
+
+bool check_file_exists(std::string filename, struct stat* file_stat) {
+    if (file_stat != NULL) {
+        return stat(filename.c_str(), file_stat) == 0;
+    }
+
+    struct stat filestat;
+    return stat(filename.c_str(), &filestat) == 0;
+}
+
+std::string join_path(std::string parent, std::string child) {
+    int path_start = child.find_first_not_of("./");
+    parent = parent.substr(0, parent.find_last_not_of("/") + 1);
+
+    if (path_start > 0 && path_start < child.length()) {
+        child = child.substr(child.find_first_not_of("./"));
+    } else if (path_start >= child.length()) {
+        child = "";
+    }
+
+    return parent + "/" + child;
+}
+
+std::string get_file_content(std::string filename) {
+    std::ifstream file(filename);
+
+    if (file.is_open()) {
+        std::string content;
+        std::string templine;
+
+        while(getline(file, templine)) {
+            content += templine + "\n";
+        }
+
+        file.close();
+
+        return content;
+    }
+
+    return "";
 }
--- src/handleRequest.hpp
+++ src/handleRequest.hpp
@@ -1,7 +1,19 @@
+#include <string>
+
 #include "http_parser/parser.hpp"
+#include "response/response.hpp"
 #include "connection.hpp"
 
 #ifndef _HANDLE_REQUEST_
 #define _HANDLE_REQUEST_
-extern void handleRequest(Connection, IncomingMessage_t);
+
+#define STATIC_DIR "./static"
+
+extern void handleRequest(Connection&, IncomingMessage_t);
+extern bool check_file_exists(std::string, struct stat*);
+extern std::string join_path(std::string, std::string);
+extern void serve_static(Response *, std::string);
+extern void not_found(Response *);
+extern std::string get_file_content(std::string);
+
 #endif
