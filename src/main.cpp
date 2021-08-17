ebfaf2fd4d504d96c58c94221767f38bddd04c4c
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-08-17 12:24:26 +0100

    Handle exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -2,53 +2,58 @@
 #include <sys/socket.h>
 #include <bits/refwrap.h>
 #include <iostream>
+#include <exception>
+#include <argp.h>
+#include <tr1/tuple>
 
 #include "config.hpp"
 #include "pool.hpp"
 #include "connection.hpp"
 #include "server.hpp"
 #include "handleThread.hpp"
+#include "MainThread.hpp"
 
 
-int main () {
-    int server_socket;
-    bool keep_alive = true; // whether to set SO_KEEPALIVE or not
-    config_t config; // Configuration object
+int main (int argc, char **argv) {
+    int server_socket; // Server Socket Fd
     ConnectionsPool connections; // Connections pool
-    socklen_t address_len = sizeof(struct sockaddr_in);
-    unsigned int accepted_connections = 0; // Keep tracking accepted_connections to implement round robin on threads
+    config_t config; // Configuration object
+    bool keep_alive = true; // whether to set SO_KEEPALIVE or not
 
-    init_config(&config); /* Initializing config object with default values */
+    try {
+        init_config(&config); /* Initializing config object with default values */
+        parse_config(&config); /* Parse Configuration from config file */
+    } catch (const char *err) {
+        std::cerr << "[CONFIG ERROR] " << err << std::endl;
+        exit(EXIT_FAILURE);
+    } catch (const std::exception &e) {
+        std::cerr << "[CONFIG ERROR] " << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-    std::thread threads_pool[config.threads_cout];
+    std::thread threads_pool[config.threads_cout]; /* Array Of Workers Threads */
 
     // Initialize threads
     for (int i = 0; i < config.threads_cout; i++) {
         threads_pool[i] = std::thread(handleThread, std::ref(connections), i, &config);
     }
 
-    // Start server
     keep_alive = config.http_version.compare("HTTP/1.0") != 0;  // No SO_KEEPALIVE on http/1.0
-    server_socket = start_server(config.address.c_str(), config.port, keep_alive);
 
-    // Keep accepting new connections
-    while (true) {
-        connections.cleanClosedConnections();
-        int ps = connections.size();
-        std::cout << "[*] Size of pool " << ps << std::endl; 
-        std::cout << "[*] Size of pool " << sizeof(Connection) * ps << std::endl; 
-
-        struct sockaddr_in client_address;
-        int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &address_len);
-
-        Connection conn(client_socket, client_address);
-        conn.worker_id = accepted_connections % config.threads_cout; 
-        connections.push_back(conn);
-
-        accepted_connections++;
-        
-        std::cout << "[*] Accepted connection num : " << accepted_connections << std::endl;
+    try {
+        // Server Starts listening here
+        server_socket = start_server(config.address.c_str(), config.port, keep_alive);
+        std::cout << "[*] Server is listening " << config.address << ":" << config.port << std::endl;
+    } catch (const char *err) {
+        std::cerr << "[ERROR STARTING SERVER] " << err << std::endl;
+        exit(EXIT_FAILURE);
+    } catch (const std::exception &e) {
+        std::cerr << "[ERROR STARTING SERVER] " << e.what() << std::endl;
+        exit(EXIT_FAILURE);
     }
+
+    // Start the main thread that Accept new connections
+    start_main_thread(server_socket, connections, config);
 
     // Wait for threads to finish in case of interopt ?
     for (int i = 0; i < config.threads_cout; i++) threads_pool[i].join();
